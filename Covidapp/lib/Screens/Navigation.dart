364d9920f18cc8135e09be21eec4d5cd1eb8294c
Rudransh <rudranshkush2000@gmail.com>
2021-09-25 23:20:53 +0530

    Bottom navigation was added

--- Covidapp/lib/Screens/Navigation.dart
+++ Covidapp/lib/Screens/Navigation.dart
@@ -7,32 +7,37 @@ import 'package:covid_infos/Screens/Dashboard/Homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 class Navigation extends StatefulWidget {
   SharedPreferences prefsnewtrial;
   Navigation({this.prefsnewtrial});
   @override
-  _NavigationState createState() => _NavigationState(prefsnewtrial:prefsnewtrial);
+  _NavigationState createState() =>
+      _NavigationState(prefsnewtrial: prefsnewtrial);
 }
 
 class _NavigationState extends State<Navigation> {
   SharedPreferences prefsnewtrial;
   _NavigationState({this.prefsnewtrial});
-  int page=0;
+  int page = 0;
   @override
   Widget build(BuildContext context) {
     List<Widget> tabs = [
-      homepage(prefsnewtrial:prefsnewtrial),
-      country(prefsnewtrial:prefsnewtrial),
-      contributionbody(),
+      homepage(prefsnewtrial: prefsnewtrial),
+      country(prefsnewtrial: prefsnewtrial),
+      /*contributionbody(),*/
       infos(),
     ];
     return Scaffold(
-      body: tabs[page],
+        body: tabs[page],
         bottomNavigationBar: FancyBottomNavigation(
           tabs: [
-            TabData(iconData: Icons.home, title: "Home",),
+            TabData(
+              iconData: Icons.home,
+              title: "Home",
+            ),
             TabData(iconData: Icons.flag, title: "Countries"),
-            TabData(iconData: Icons.share, title: "Contribution"),
+            /*TabData(iconData: Icons.share, title: "Contribution"),*/
             TabData(iconData: Icons.info, title: "Info"),
           ],
           onTabChangedListener: (position) {
@@ -40,7 +45,6 @@ class _NavigationState extends State<Navigation> {
               page = position;
             });
           },
-        )
-    );
+        ));
   }
 }
